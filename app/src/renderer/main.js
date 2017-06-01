03231a0a185e68e73c3b4187507506e4a4a01419
gengjiawen <technicalcute@gmail.com>
2017-06-01 19:31:05 +0800

    add vue-rx

--- app/src/renderer/main.js
+++ app/src/renderer/main.js
@@ -2,17 +2,19 @@ import Vue from 'vue'
 import Electron from 'vue-electron'
 import Resource from 'vue-resource'
 import Router from 'vue-router'
-
+import Rx from 'rxjs/Rx'
+import VueRx from 'vue-rx'
 import App from './App'
 import routes from './routes'
-
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 
+// tada!
 Vue.use(Electron)
 Vue.use(Resource)
 Vue.use(Router)
 Vue.use(ElementUI)
+Vue.use(VueRx, Rx)
 Vue.config.debug = true
 
 const router = new Router({
